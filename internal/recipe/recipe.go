fc844dc533f56d5f19582c9eaa4968d2353adf1b
Keith Thibodeaux <keith@railscoder.net>
2025-04-18 06:02:23 -0400

    dependency struct

--- internal/recipe/recipe.go
+++ internal/recipe/recipe.go
@@ -3,10 +3,10 @@ package recipe
 type Recipe struct {
 	CookbookCategory string `toml:"category"`
 	ID               string
-	Name             string `toml:"name"`
-	Parts            []Part `toml:"groups"`
-	Source           string `toml:"source"`
-	DependencyIDs    []string
+	Name             string       `toml:"name"`
+	Parts            []Part       `toml:"groups"`
+	Source           string       `toml:"source"`
+	Dependencies     []Dependency `toml:"dependencies"`
 }
 
 type Part struct {
@@ -15,4 +15,9 @@ type Part struct {
 	Name        string   `toml:"name"`
 	PrepTime    int      `toml:"prep_time"`
 	Steps       []string `toml:"steps"`
+}
+
+type Dependency struct {
+	RecipeID   string `toml:"recipe_id"`
+	IsRequired bool   `toml:"is_required"`
 }
