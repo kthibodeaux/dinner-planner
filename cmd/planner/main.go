2700d4e18db789fed8d3b6f5fd449db78b800a9a
Keith Thibodeaux <keith@railscoder.net>
2025-04-26 10:44:46 -0400

    planner skeleton

--- cmd/planner/main.go
+++ cmd/planner/main.go
@@ -2,34 +2,18 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"time"
 
 	"github.com/kthibodeaux/dinner-planner/internal/config"
 	"github.com/kthibodeaux/dinner-planner/internal/recipe"
+	"github.com/kthibodeaux/dinner-planner/internal/utils"
 )
 
 func main() {
 	config := config.LoadConfig()
 
 	recipes := recipe.Load(config.RecipeDirectory)
-	fmt.Printf("Loaded %d recipes", len(recipes))
-	fmt.Println()
-	dates := loadDates(config.StartDate)
+	dates := utils.DatesForWeekStartingOn(config.StartDate)
 
+	fmt.Println("Recipes:", len(recipes))
 	fmt.Println("Dates:", dates)
-}
-
-func loadDates(startDate string) []time.Time {
-	date, err := time.Parse("2006-01-02", startDate)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	dates := make([]time.Time, 7)
-	for i := range 7 {
-		dates[i] = date.AddDate(0, 0, i)
-	}
-
-	return dates
 }
