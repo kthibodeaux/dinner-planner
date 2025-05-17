76dfe3461b2e2d54b87d35969452871698b4c9fc
Keith Thibodeaux <keith@railscoder.net>
2025-05-17 17:10:18 -0400

    refactor

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"github.com/kthibodeaux/dinner-planner/internal/config"
 	"github.com/kthibodeaux/dinner-planner/internal/web"
 )
 
 func main() {
-	web.Serve(config.Get().RecipeDirectory, config.Get().Web.Port)
+	web.Serve()
 }
