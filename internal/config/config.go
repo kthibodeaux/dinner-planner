703f1df87dd9afa1413c46ed36d50fcf7169d5bd
Keith Thibodeaux <keith@railscoder.net>
2025-05-17 17:06:47 -0400

    refactor

--- internal/config/config.go
+++ internal/config/config.go
@@ -110,72 +110,34 @@ func loadConfig() (*Config, error) {
 		config.Web.Port = flagWebPort
 	}
 
-	if config.Planner.Color == "" {
-		config.Planner.Color = "#3A5B7E"
-	}
-	if config.Planner.FirstDayOfWeek == "" {
-		config.Planner.FirstDayOfWeek = "sunday"
-	}
-	if config.RecipeDirectory == "" {
-		config.RecipeDirectory = expandHome("~/recipes")
-	}
+	defaultIfEmpty(&config.Planner.Color, "#3A5B7E")
+	defaultIfEmpty(&config.Planner.FirstDayOfWeek, "sunday")
+	defaultIfEmpty(&config.RecipeDirectory, expandHome("~/recipes"))
 	if config.StartDate == "" {
 		config.StartDate, err = startOfWeek(time.Now(), config.Planner.FirstDayOfWeek)
 		if err != nil {
 			return nil, err
 		}
 	}
-	if config.Web.Port == "" {
-		config.Web.Port = defaultWebPort
-	}
+	defaultIfEmpty(&config.Web.Port, defaultWebPort)
 	if config.Web.Port[0] != ':' {
 		config.Web.Port = ":" + config.Web.Port
 	}
 
-	// Defaults for keys
-	k := &config.Planner.Keys
-	if k.Quit == "" {
-		k.Quit = "ctrl+c"
-	}
-	if k.MainView == "" {
-		k.MainView = "q"
-	}
-	if k.Focus == "" {
-		k.Focus = "f"
-	}
-	if k.Help == "" {
-		k.Help = "h"
-	}
-	if k.Recipes == "" {
-		k.Recipes = "0"
-	}
-	if k.Down == "" {
-		k.Down = "j"
-	}
-	if k.Up == "" {
-		k.Up = "k"
-	}
-	if k.Day1 == "" {
-		k.Day1 = "1"
-	}
-	if k.Day2 == "" {
-		k.Day2 = "2"
-	}
-	if k.Day3 == "" {
-		k.Day3 = "3"
-	}
-	if k.Day4 == "" {
-		k.Day4 = "4"
-	}
-	if k.Day5 == "" {
-		k.Day5 = "5"
-	}
-	if k.Day6 == "" {
-		k.Day6 = "6"
-	}
-	if k.Day7 == "" {
-		k.Day7 = "7"
-	}
+	defaultIfEmpty(&config.Planner.Keys.Quit, "ctrl+c")
+	defaultIfEmpty(&config.Planner.Keys.MainView, "q")
+	defaultIfEmpty(&config.Planner.Keys.Focus, "f")
+	defaultIfEmpty(&config.Planner.Keys.Help, "h")
+	defaultIfEmpty(&config.Planner.Keys.Recipes, "0")
+	defaultIfEmpty(&config.Planner.Keys.Down, "j")
+	defaultIfEmpty(&config.Planner.Keys.Up, "k")
+	defaultIfEmpty(&config.Planner.Keys.Day1, "1")
+	defaultIfEmpty(&config.Planner.Keys.Day2, "2")
+	defaultIfEmpty(&config.Planner.Keys.Day3, "3")
+	defaultIfEmpty(&config.Planner.Keys.Day4, "4")
+	defaultIfEmpty(&config.Planner.Keys.Day5, "5")
+	defaultIfEmpty(&config.Planner.Keys.Day6, "6")
+	defaultIfEmpty(&config.Planner.Keys.Day7, "7")
 
 	return config, nil
 }
@@ -239,5 +201,11 @@ func (c *Config) DayKeyMap() map[int]string {
 		4: c.Planner.Keys.Day5,
 		5: c.Planner.Keys.Day6,
 		6: c.Planner.Keys.Day7,
+	}
+}
+
+func defaultIfEmpty(val *string, def string) {
+	if *val == "" {
+		*val = def
 	}
 }
