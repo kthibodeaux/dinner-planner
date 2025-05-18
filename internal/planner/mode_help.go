0700790f005b809700d6a8d399fc7c39d76022fa
Keith Thibodeaux <keith@railscoder.net>
2025-05-18 17:08:55 -0400

    comprehensive key list in help

--- internal/planner/mode_help.go
+++ internal/planner/mode_help.go
@@ -19,17 +19,27 @@ func (dp *dinnerPlan) viewModeHelp() string {
 		Render(title + "\n" + subtitle + "\n\n" + keyBindings + "\n")
 
 	content := header +
-		"Focus:        " + config.Get().Planner.Keys.Focus + "\n" +
-		"Help:         " + config.Get().Planner.Keys.Help + "\n" +
-		"Recipes:      " + config.Get().Planner.Keys.Recipes + "\n" +
-		"Day 1:        " + config.Get().Planner.Keys.Day1 + "\n" +
-		"Day 2:        " + config.Get().Planner.Keys.Day2 + "\n" +
-		"Day 3:        " + config.Get().Planner.Keys.Day3 + "\n" +
-		"Day 4:        " + config.Get().Planner.Keys.Day4 + "\n" +
-		"Day 5:        " + config.Get().Planner.Keys.Day5 + "\n" +
-		"Day 6:        " + config.Get().Planner.Keys.Day6 + "\n" +
-		"Day 7:        " + config.Get().Planner.Keys.Day7 + "\n" +
-		"Quit:         " + config.Get().Planner.Keys.Quit
+		"Help:           " + config.Get().Planner.Keys.Help + "\n" +
+		"Main:           " + config.Get().Planner.Keys.MainView + "\n" +
+		"Shopping List:  " + config.Get().Planner.Keys.ShoppingList + "\n" +
+		"> Toggle:       " + config.Get().Planner.Keys.ShoppingListToggle + "\n" +
+		"\n" +
+		"Up:             " + config.Get().Planner.Keys.Up + "\n" +
+		"Down:           " + config.Get().Planner.Keys.Down + "\n" +
+		"Scroll Up:      " + config.Get().Planner.Keys.ScrollUp + "\n" +
+		"Scroll Down:    " + config.Get().Planner.Keys.ScrollDown + "\n" +
+		"\n" +
+		"Focus:          " + config.Get().Planner.Keys.Focus + "\n" +
+		"Recipes:        " + config.Get().Planner.Keys.Recipes + "\n" +
+		"Day 1:          " + config.Get().Planner.Keys.Day1 + "\n" +
+		"Day 2:          " + config.Get().Planner.Keys.Day2 + "\n" +
+		"Day 3:          " + config.Get().Planner.Keys.Day3 + "\n" +
+		"Day 4:          " + config.Get().Planner.Keys.Day4 + "\n" +
+		"Day 5:          " + config.Get().Planner.Keys.Day5 + "\n" +
+		"Day 6:          " + config.Get().Planner.Keys.Day6 + "\n" +
+		"Day 7:          " + config.Get().Planner.Keys.Day7 + "\n" +
+		"\n" +
+		"Quit:           " + config.Get().Planner.Keys.Quit
 
 	pane := dp.stylePaneBorder(borderForce).
 		Width(width).
